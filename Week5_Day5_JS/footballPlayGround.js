d78c51c498a202e341b9af10f0fa318c4a8b2cd9
BIT Student <classroom@bgit.rs>
2019-11-08 12:55:31 +0100

    events

---
+++ Week5_Day5_JS/footballPlayGround.js
@@ -0,0 +1,6 @@
+Create page which has soccer field as bg image
+In top left corner there should be image of soccer player
+When user clicks anywhere on the field, the player is moved to that position
+Add the button which is used to disable player movement
+
+
